4194a3bacdc7f7cd973ef2579999147e59b6b7a9
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-02-02 16:37:24 -0500

    Assignment 3 Part 1 implement repeat, getClock, and toMillisec

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.util;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -57,7 +58,26 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
+        pause();
+        for (int i = 0; i < n; i++) {
+            // prepare data
+            T data = supplier.get();
+            // pre-processes
+            if (preFunction != null) {
+                data = preFunction.apply(data);
+            }
+            // run
+            resume();
+            U u = function.apply(data);
+            pauseAndLap();
+            // post-processes
+            if (postFunction != null) {
+                postFunction.accept(u);
+            }
+        }
+        final double result = meanLapTime();
+        resume();
+        return result;
         // END 
     }
 
@@ -177,7 +197,7 @@ public class Timer {
      */
     private static long getClock() {
         // FIXME by replacing the following code
-         return 0;
+         return System.nanoTime();
         // END 
     }
 
@@ -190,7 +210,7 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // FIXME by replacing the following code
-         return 0;
+         return ticks / 1000000.0;
         // END 
     }
 
