fe4e4998828a2d23b75f86bf34949361fe0f3aa2
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-02-04 16:01:14 -0500

    Assignment 3 Part 3 Benchmark

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
@@ -6,9 +6,13 @@ package edu.neu.coe.info6205.sort.elementary;
 import edu.neu.coe.info6205.sort.BaseHelper;
 import edu.neu.coe.info6205.sort.Helper;
 import edu.neu.coe.info6205.sort.SortWithHelper;
+import edu.neu.coe.info6205.util.Benchmark;
+import edu.neu.coe.info6205.util.Benchmark_Timer;
 import edu.neu.coe.info6205.util.Config;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
@@ -83,5 +87,78 @@ public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
     public static <T extends Comparable<T>> void sort(T[] ts) {
         new InsertionSort<T>().mutatingSort(ts);
+    }
+
+    public static void main(String[] args) {
+        /*int size = 20;
+        Integer[] input = new Integer[size];
+        Random rd = new Random();
+        for (int j = 0; j < size; j++) {
+            input[j] = rd.nextInt();
+        }
+        Integer[] inputSorted = Arrays.copyOf(input, size);
+        Arrays.sort(inputSorted);
+        Integer[] inputReversed = Arrays.copyOf(input, size);
+        Arrays.sort(inputReversed, Collections.reverseOrder());
+        Integer[] inputHalfSorted = Arrays.copyOf(input, size);
+        Arrays.sort(inputHalfSorted, 0, size/2);
+        System.out.println("Random");
+        for (Integer integer : input) {
+            System.out.println(integer);
+        }
+        System.out.println();
+        System.out.println("Sorted");
+        for (Integer integer : inputSorted) {
+            System.out.println(integer);
+        }
+        System.out.println();
+        System.out.println("Reversed");
+        for (Integer integer : inputReversed) {
+            System.out.println(integer);
+        }
+        System.out.println();
+        System.out.println("HalfSorted");
+        for (Integer integer : inputHalfSorted) {
+            System.out.println(integer);
+        }*/
+
+        int size = 2500, m = 100;
+        for(int i = 0; i < 6; i++) {
+            // generate input
+            size *= 2;
+            Integer[] input = new Integer[size];
+            Random rd = new Random();
+            for (int j = 0; j < size; j++) {
+                input[j] = rd.nextInt();
+            }
+            Integer[] inputSorted = Arrays.copyOf(input, size);
+            Arrays.sort(inputSorted);
+            Integer[] inputReversed = Arrays.copyOf(input, size);
+            Arrays.sort(inputReversed, Collections.reverseOrder());
+            Integer[] inputHalfSorted = Arrays.copyOf(input, size);
+            Arrays.sort(inputHalfSorted, 0, size/2);
+            Consumer<Integer[]> fRun = b -> InsertionSort.sort(b);
+            UnaryOperator<Integer[]> fPre = b -> Arrays.copyOf(b, b.length);
+            // test random
+            String randomArrayDescription = "Test insertion sort on random array. Size: " + size;
+            Benchmark<Integer[]> benchmark_timer_random = new Benchmark_Timer<>(randomArrayDescription,fPre,fRun);
+            double time_random = benchmark_timer_random.run(input, m);
+            System.out.println(time_random);
+            // test sorted
+            String sortedArrayDescription = "Test insertion sort on sorted array. Size: " + size;
+            Benchmark<Integer[]> benchmark_timer_sorted = new Benchmark_Timer<>(randomArrayDescription,fPre,fRun);
+            double time_sorted = benchmark_timer_sorted.run(inputSorted, m);
+            System.out.println(time_sorted);
+            // test reversed
+            String reversedArrayDescription = "Test insertion sort on reversed array. Size: " + size;
+            Benchmark<Integer[]> benchmark_timer_reversed = new Benchmark_Timer<>(randomArrayDescription,fPre,fRun);
+            double time_reversed = benchmark_timer_reversed.run(inputReversed, m);
+            System.out.println(time_reversed);
+            // test half sorted
+            String halfSortedArrayDescription = "Test insertion sort on half sorted array. Size: " + size;
+            Benchmark<Integer[]> benchmark_timer_half_sorted = new Benchmark_Timer<>(randomArrayDescription,fPre,fRun);
+            double time_half_sorted = benchmark_timer_half_sorted.run(inputHalfSorted, m);
+            System.out.println(time_half_sorted);
+        }
     }
 }
